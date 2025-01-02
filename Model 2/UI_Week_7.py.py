6c0c71096c33917f3a6196b46ec99cc862c0d9de
azeemanwar789 <131450922+azeemanwar789@users.noreply.github.com>
2025-01-03 00:20:32 +0530

    Add files via upload

---
+++ Model 2/UI_Week_7.py.py
@@ -0,0 +1,130 @@
+import streamlit as st
+import numpy as np
+from joblib import load
+from sklearn.linear_model import LogisticRegression
+import time
+
+# Path to your trained Logistic Regression model .joblib file
+model_file = r"C:\Azeem's Work\IDC Internship\2nd Month\Report-Tasks\Week 7&8 Task Material\lr_disease_prediction_model.joblib"
+
+# Load the trained Logistic Regression model
+model = load(model_file)
+
+# Verify the model type
+if not isinstance(model, LogisticRegression):
+    st.error('The loaded model is not of the expected type: LogisticRegression.')
+else:
+    st.write(f'Model loaded successfully: {type(model)}')
+
+# Disease names mapping
+disease_names = {
+    0: '(vertigo) Paroymsal Positional Vertigo',
+    1: 'AIDS',
+    2: 'Acne',
+    3: 'Alcoholic hepatitis',
+    4: 'Allergy',
+    5: 'Arthritis',
+    6: 'Bronchial Asthma',
+    7: 'Cervical spondylosis',
+    8: 'Chicken pox',
+    9: 'Chronic cholestasis',
+    10: 'Common Cold',
+    11: 'Dengue',
+    12: 'Diabetes ',
+    13: 'Dimorphic hemmorhoids(piles)',
+    14: 'Drug Reaction',
+    15: 'Fungal infection',
+    16: 'GERD',
+    17: 'Gastroenteritis',
+    18: 'Heart attack',
+    19: 'Hepatitis B',
+    20: 'Hepatitis C',
+    21: 'Hepatitis D',
+    22: 'Hepatitis E',
+    23: 'Hypertension ',
+    24: 'Hyperthyroidism',
+    25: 'Hypoglycemia',
+    26: 'Hypothyroidism',
+    27: 'Impetigo',
+    28: 'Jaundice',
+    29: 'Malaria',
+    30: 'Migraine',
+    31: 'Osteoarthristis',
+    32: 'Paralysis (brain hemorrhage)',
+    33: 'Peptic ulcer disease',
+    34: 'Pneumonia',
+    35: 'Psoriasis',
+    36: 'Tuberculosis',
+    37: 'Typhoid',
+    38: 'Urinary tract infection',
+    39: 'Varicose veins',
+    40: 'Hepatitis A'
+}
+
+# Predefined 17 model input features
+model_features = [
+    'itching', 'skin_rash', 'nodal_skin_eruptions', 'continuous_sneezing',
+    'shivering', 'chills', 'joint_pain', 'stomach_pain', 'acidity',
+    'ulcers_on_tongue', 'vomiting', 'fatigue', 'weight_loss', 'anxiety',
+    'cold_hands_and_feets', 'mood_swings', 'restlessness'
+]
+
+# All 100 symptoms
+all_symptoms = [
+    'itching', 'skin_rash', 'nodal_skin_eruptions', 'continuous_sneezing', 'shivering', 'chills',
+    'joint_pain', 'stomach_pain', 'acidity', 'ulcers_on_tongue', 'muscle_wasting', 'vomiting',
+    'burning_micturition', 'spotting_urination', 'fatigue', 'weight_gain', 'anxiety',
+    'cold_hands_and_feets', 'mood_swings', 'weight_loss', 'restlessness', 'lethargy',
+    'patches_in_throat', 'irregular_sugar_level', 'cough', 'high_fever', 'sunken_eyes',
+    'breathlessness', 'sweating', 'dehydration', 'indigestion', 'headache', 'yellowish_skin',
+    'dark_urine', 'nausea', 'loss_of_appetite', 'pain_behind_the_eyes', 'back_pain',
+    'constipation', 'abdominal_pain', 'diarrhoea', 'mild_fever', 'yellow_urine',
+    'yellowing_of_eyes', 'acute_liver_failure', 'fluid_overload', 'swelling_of_stomach',
+    'swelled_lymph_nodes', 'malaise', 'blurred_and_distorted_vision', 'phlegm',
+    'throat_irritation', 'redness_of_eyes', 'sinus_pressure', 'runny_nose', 'congestion',
+    'chest_pain', 'weakness_in_limbs', 'fast_heart_rate', 'pain_during_bowel_movements',
+    'pain_in_anal_region', 'bloody_stool', 'irritation_in_anus', 'neck_pain', 'dizziness',
+    'cramps', 'bruising', 'obesity', 'swollen_legs', 'swollen_blood_vessels',
+    'puffy_face_and_eyes', 'enlarged_thyroid', 'brittle_nails', 'swollen_extremeties',
+    'excessive_hunger', 'extra_marital_contacts', 'drying_and_tingling_lips', 'slurred_speech',
+    'knee_pain', 'hip_joint_pain', 'muscle_weakness', 'stiff_neck', 'swelling_joints',
+    'movement_stiffness', 'spinning_movements', 'loss_of_balance', 'unsteadiness',
+    'weakness_of_one_body_side', 'loss_of_smell', 'bladder_discomfort', 'foul_smell_ofurine',
+    'continuous_feel_of_urine', 'passage_of_gases', 'internal_itching', 'toxic_look_(typhos)',
+    'depression', 'irritability', 'muscle_pain', 'altered_sensorium', 'red_spots_over_body',
+    'belly_pain', 'abnormal_menstruation', 'dischromic_patches', 'watering_from_eyes',
+    'increased_appetite', 'polyuria', 'family_history', 'mucoid_sputum', 'rusty_sputum',
+    'lack_of_concentration', 'visual_disturbances', 'receiving_blood_transfusion',
+    'receiving_unsterile_injections', 'coma', 'stomach_bleeding', 'distention_of_abdomen',
+    'history_of_alcohol_consumption', 'fluid_overload', 'blood_in_sputum', 'prominent_veins_on_calf',
+    'palpitations', 'painful_walking', 'pus_filled_pimples', 'blackheads', 'scurring',
+    'skin_peeling', 'silver_like_dusting', 'small_dents_in_nails', 'inflammatory_nails',
+    'blister', 'red_sore_around_nose', 'yellow_crust_ooze', 'prognosis'
+]
+
+# Streamlit Interface
+st.title('Disease Prediction Based on Symptoms')
+
+# User inputs: Select symptoms
+selected_symptoms = st.multiselect("Select Symptoms", all_symptoms)
+
+# Map selected symptoms to model features
+def map_symptoms_to_features(selected_symptoms, model_features):
+    feature_vector = [1 if feature in selected_symptoms else 0 for feature in model_features]
+    return np.array(feature_vector).reshape(1, -1)
+
+# Predict disease if symptoms are selected
+if selected_symptoms:
+    user_input = map_symptoms_to_features(selected_symptoms, model_features)
+    
+    # Show progress
+    with st.spinner('Making the prediction...'):
+        time.sleep(2)
+        predicted_label = model.predict(user_input)
+        predicted_disease = disease_names[predicted_label[0]]
+        
+        # Display prediction
+        st.subheader('Predicted Disease:')
+        st.write(f'The model predicts: {predicted_disease}')
+else:
+    st.write('Please select some symptoms to predict the disease.')
